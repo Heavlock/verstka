0918250b3df1a87cf797cadd5919b8dfbfc7fbcb
Heavlock <33984264+Heavlock@users.noreply.github.com>
2020-07-06 14:45:43 +0500

    тест

--- #diplom/js/scrypt.js
+++ #diplom/js/scrypt.js
@@ -1,23 +1,21 @@
-
-window.onload = function() {
-    let test = document.querySelectorAll('.fly-elements');
-    console.log(test);
- test.forEach(test => test.classList.add("animate"));
- };
- function testWebP(callback) {
-
-    var webP = new Image();
-    webP.onload = webP.onerror = function () {
+window.onload = function () {
+  let test = document.querySelectorAll('.fly-elements');
+  console.log(test);
+  test.forEach((test) => test.classList.add('animate'));
+};
+function testWebP(callback) {
+  var webP = new Image();
+  webP.onload = webP.onerror = function () {
     callback(webP.height == 2);
-    };
-    webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
-    }
-    
-    testWebP(function (support) {
-    
-    if (support == true) {
+  };
+  webP.src =
+    'data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA';
+}
+
+testWebP(function (support) {
+  if (support == true) {
     document.querySelector('body').classList.add('webp');
-    }else{
+  } else {
     document.querySelector('body').classList.add('no-webp');
-    }
-    });
+  }
+});
